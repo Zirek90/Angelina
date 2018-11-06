45ed9a907e3b28633baff5847a00f9e4b996c5fd
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-11-06 14:19:45 +0100

    added slider

--- js/main.js
+++ js/main.js
@@ -2,16 +2,75 @@ $(function () {
     
     const leftArrow = $(".left-arrow");
     const rightArrow = $(".right-arrow");
-    const rightBar = $(".right-container")
     
-     showLeftBar = () => {
+    // Left bar functions
+    showLeftBar = () => {
         $(".left-container").toggle('display')
     };
+    hideLeftArrowText = () => {
+        $(".left-tooltip").css('display', 'none');
+        $(".left-arrow-container").css("left", "25%");
+    }
+    showLeftArrowText = () => {
+        $(".left-tooltip").css('display', 'block');
+        $(".left-arrow-container").css("left", "26%");
+    }
+    
+
+    // Right bar functions
     showRightBar = () => {
         $(".right-container").toggle('display')
     };
+    hideRightArrowText = () => {
+        $(".right-tooltip").css('display', 'none');
+        $(".right-arrow-container").css("right", "25%");
+    }
+    showRightArrowText = () => {
+        $(".right-tooltip").css('display', 'block');
+        $(".right-arrow-container").css("right", "26%");
+    }
 
     leftArrow.on("click", showLeftBar);
+    leftArrow.on("mouseover", hideLeftArrowText);
+    leftArrow.on("mouseout", showLeftArrowText);
     rightArrow.on("click", showRightBar);
+    rightArrow.on("mouseover", hideRightArrowText);
+    rightArrow.on("mouseout", showRightArrowText);
+
+    // move to gallery
+
+    moveToGallery = () => {
+        $('.wrapper').css('display', 'none');
+        $('.slider-container').css('display', 'block');
+    }
+
+    $('.gallery').on('click', moveToGallery);
+
+
+    //slider
+
+    nextImage = () => {
+        const currentImg = $('.active');
+        const nextImg = currentImg.next();
+
+        if (nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    }
+
+    prevImage = () => {
+        const currentImg = $('.active');
+        const prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+    }
+
+   $('#nextPicture').on('click', nextImage);
+   $('#prevPicture').on('click', prevImage);
+
 
 })
